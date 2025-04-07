264bcbd7205697e8747c7f1d817ec8dbbbf8f7eb
Erik López <erik16824@gmail.com>
2025-04-06 22:37:07 -0500

    Corregir importación en admin.py

--- scraper/admin.py
+++ scraper/admin.py
@@ -1,19 +1,6 @@
 from django.contrib import admin
-from .models import Partido, Candidato, ResultadoElectoral
+from .models import Candidato, Partido, Resultado
 
-@admin.register(Partido)
-class PartidoAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'lista')
-    search_fields = ('nombre', 'lista')
-
-@admin.register(Candidato)
-class CandidatoAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'cargo', 'partido')
-    list_filter = ('cargo', 'partido')
-    search_fields = ('nombre', 'cargo')
-
-@admin.register(ResultadoElectoral)
-class ResultadoElectoralAdmin(admin.ModelAdmin):
-    list_display = ('candidato', 'provincia', 'votos', 'porcentaje', 'fecha_actualizacion')
-    list_filter = ('provincia', 'candidato__partido')
-    search_fields = ('candidato__nombre', 'provincia')
+admin.site.register(Candidato)
+admin.site.register(Partido)
+admin.site.register(Resultado)
